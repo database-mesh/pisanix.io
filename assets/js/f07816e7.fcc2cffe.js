5319c2ec3be3771d276fcfea451d202aba775d5c
mlycore <maxwell92@126.com>
2022-04-26 17:46:20 +0800

    fix: foot in english version

---
+++ assets/js/f07816e7.fcc2cffe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpisanix=self.webpackChunkpisanix||[]).push([[155],{3769:function(s){s.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
---
+++ en/assets/js/03b8d21d.4830cd34.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpisanix=self.webpackChunkpisanix||[]).push([[976],{4469:function(n){n.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-blog","id":"default"}}')}}]);
